be40406be78fe0c99efdbf32e720cf12f3cefd8f
Shahid Ullah <114725262+ShahidUllahSE@users.noreply.github.com>
2025-05-30 16:19:51 +0500

    updated

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,9 +1,12 @@
+// config/cloudinary.ts
+
 import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 
+// Configure Cloudinary with environment variables
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
-  api_key: process.env.CLOUDINARY_API_KEY || "",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "",
+  cloud_name: process.env.CLOUDINARY_NAME ?? "",
+  api_key: process.env.CLOUDINARY_API_KEY ?? "",
+  api_secret: process.env.CLOUDINARY_API_SECRET ?? "",
 });
 
 export default cloudinary;
